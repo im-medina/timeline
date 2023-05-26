af523aa77ba7e52958a54790b6b8f6de5b16677d
Medger <dnt.1554@gmail.com>
2023-05-26 00:32:07 -0300

    edited web and mobile version, both with image problems

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import {Text, View } from 'react-native';
+import {ImageBackground, Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '.src/assets/bg-blur.png'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if(!hasLoadedFonts){
+    return null
+  }
+
   return (
-    <View className="bg-gray-950 flex-auto items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-5xl">Hello World</Text>
+    <ImageBackground
+    source={blurBg}
+    className="relative flex-1 bg-gray-900"
+    imageStyle={{ position: 'absolute', left: '-100%' }}
+  >
+
       <StatusBar style="light" />
-    </View>
+    </ImageBackground>
   );
 }
 
