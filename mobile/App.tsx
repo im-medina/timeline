d89ebecfca559f9de5744f0e6bdb674d102f211a
Medger <dnt.1554@gmail.com>
2023-05-28 13:53:23 -0300

    logo and centrilized items fixed on mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -12,6 +12,10 @@ import {
 
 import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
+import NLWlogo from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind';
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -27,10 +31,15 @@ export default function App() {
   return (
     <ImageBackground
     source={blurBg}
-    className="relative flex-1 bg-gray-900"
+    className="relative flex-1 items-center bg-gray-900"
     imageStyle={{ position: 'absolute', left: '-150%' }}
   >
-    <Stripes/>
+     <StyledStripes className="absolute left-2" />
+
+    <View className="flex-1 items-center justify-center gap-6">
+      <NLWlogo className="flex"/>
+      <Text className="text-gray-50">Sua capsula do tempo</Text>
+    </View>
 
       <StatusBar style="light" />
     </ImageBackground>
